5ba5256ba521e417f893a31bc8573954bb5140ad
Mehul2001 <mmandviya@mail.csuchico.edu>
2019-01-16 21:20:23 -0800

    Implment the grocery list with two elements in the list

--- app.js
+++ app.js
@@ -1,1 +1,14 @@
-// TODO
+var GroceryList = () => (
+  <div>
+    <h1>GroceryList</h1>
+      <ul>
+          <li>Orange</li>
+          <li>Banana</li>
+      </ul>
+  </div>
+ )
+ 
+ 
+ 
+ 
+ ReactDOM.render(<GroceryList />, document.getElementById('app'));
